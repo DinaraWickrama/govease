890852880342ad806c8cececb7211260a689b886
DinaraWickrama <thiyuminidinara@gmail.com>
2025-08-16 08:19:22 +0530

    Add files via upload

---
+++ app/Welcome.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/login');
+    }, 3000); // 3 seconds delay
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [router]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex-grow flex items-center justify-center">
+        {/* This is a placeholder for the logo */}
+        <Image src="/Gove ease.png" alt="Gove ease logo" width={192} height={192} />
+      </div>
+      <div className="pb-10">
+        <p className="text-black text-lg">All Government services in one hand</p>
+      </div>
+    </main>
+  );
+}
